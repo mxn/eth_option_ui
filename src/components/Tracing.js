01847ef5d140d5b9a6e6337c42a0c9d35a889413
Maxim <mnovoseltsev@gmail.com>
2018-09-14 13:44:31 +0200

    clean getContractInstance

--- src/components/Tracing.js
+++ src/components/Tracing.js
@@ -3,16 +3,12 @@ import {
   getDaiInstance
 } from './Core'
 
-const jsonMockOasisDirect = require("solidity-contracts/MockOasisDirect.json") 
 
 
 export const dumpValues = async () => {
-  let mockOasisDirect = await getContractInstance(jsonMockOasisDirect)
-  console.log("mockOasisDirect.address", mockOasisDirect.address)
   let weth = await getWethInstance()
   let dai = await getDaiInstance()
   let wethBal = await getBalance(weth.address, mockOasisDirect.address)
   let daiBal = await getBalance(dai.address, mockOasisDirect.address)
   console.log("balances", [wethBal, daiBal])
-  
 }
